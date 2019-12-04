2f6ae8c1e5f6ebfb980e9beb1d0dbcd53f0a9e62
Volodymyr Popov <s17628@pjwstk.edu.pl>
2019-12-04 16:48:49 +0100

    Add project files.

---
+++ Pizza/Controllers/PizzaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pizza.Models;
+
+namespace Pizza.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        private s17628Context _context;
+        public PizzaController(s17628Context context)
+        {
+            _context = context;
+        }
+
+        public void GetEmpts()
+        {
+            return Ok()
+        }
+    }
+}
